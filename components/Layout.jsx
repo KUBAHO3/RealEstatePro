88b621d739164bb03c61a5a1cda4bd8f26556332
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-02-02 16:06:44 +0530

    Navbar and Footer added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 import { Children } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 
 
@@ -11,13 +13,13 @@ const Layout = ({ children }) =>(
         </Head>
         <Box maxWidth="1280px" m="auto">
             <header>
-                Navbar
+                <Navbar/>
             </header>
             <main>
                 {children}
             </main>
             <footer>
-                Footer
+                <Footer/>
             </footer>
         </Box>
     </>
