739750a4250eff86c8b9a3c2702746628de09648
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-02-03 18:57:28 +0530

    Advanced search added

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -4,10 +4,16 @@ import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
 import { filterData, getFilterValues } from "@/utils/filterData";
+import { baseUrl, fetchApi } from "@/utils/fetchApi";
+import noresult from "assets/images/noresult.svg"
 
 const SearchFilters = () =>{
     const [filters, setFilters] = useState(filterData);
-    
+    const [showLocations, setShowLocations] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [loading, setLoading] = useState(false)
+    const [locationData, setLocationData ] = useState();
+
     const router = useRouter();
     const searchProperties = (filterValues) => {
         const path = router.pathname;
@@ -16,11 +22,26 @@ const SearchFilters = () =>{
         const values = getFilterValues(filterValues)
 
         values.forEach((item) => {
-            query[item.name] = item.value
+            if(item.value && filterValues?.[item.name]) {
+                query[item.name] = item.value
+              }
         })
 
-        router.push({ pathname: path, query })
+        router.push({ pathname: path, query: query })
     }
+
+    useEffect(() => {
+        if (searchTerm !== '') {
+          const fetchData = async () => {
+            setLoading(true);
+            const data = await fetchApi(`${baseUrl}/auto-complete?query=${searchTerm}`);
+            setLoading(false);
+            setLocationData(data?.hits);
+          };
+    
+          fetchData();
+        }
+      }, [searchTerm]);
 
     return(
         <Flex bg="gray.50" p="4" justifyContent="center" flexWrap="wrap">
@@ -35,6 +56,45 @@ const SearchFilters = () =>{
                     </Select>
                 </Box>
             ))}
+            <Flex flexDir="column">
+                <Button onClick={()=> setShowLocations(!showLocations)} border="1px" borderColor="gray.200" marginTop="2">
+                    Search Location
+                </Button>
+                {showLocations && (
+                    <Flex flexDir="column" pos="relative" paddingTop="2">
+                        <Input placeholder="Type here" value={searchTerm} w="300px" 
+                                focusBorderColor="gray.300" onChange={(e) => setSearchTerm(e.target.value)}/>
+                    {searchTerm !=="" &&(
+                        <Icon as={MdCancel} pos="absolute" cursor="pointer" right="5" top="5"
+                                zIndex="100" onClick={()=>setSearchTerm("")}/>
+                    )}
+                    {loading && <Spinner margin="auto" marginTop="3" />}
+                    {showLocations &&(
+                        <Box height="300px" overflow="auto">
+                            {locationData?.map((location)=>(
+                                <Box key={location.id} onClick={()=>{
+                                    searchProperties({ locationExternalIDs: location.externalID });
+                                    setShowLocations(false);
+                                    setSearchTerm(location.name)
+                                }}>
+                                    <Text cursor="pointer" bg="gray.200" p="2" borderBottom="1px" borderColor="gray.100">
+                                        {location.name}
+                                    </Text>
+                                </Box>
+                            ))}
+                            {!loading && !locationData?.length &&(
+                                <Flex justifyContent="center" alignItems="center" flexDir="column" marginTop="5" marginBottom="5">
+                                    <Image src={noresult}/>
+                                    <Text fontSize="xl" marginTop="3">
+                                        Please Search Some!
+                                    </Text>
+                                </Flex>
+                            )}
+                        </Box>
+                    )}
+                    </Flex>
+                )}
+            </Flex>
         </Flex>
     )
 }
