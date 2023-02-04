3351c03056cfa46962384a68c8c159f655c122f3
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-02-05 00:48:04 +0530

    Property page added

--- components/Property.jsx
+++ components/Property.jsx
@@ -7,8 +7,8 @@ import { GoVerified } from "react-icons/go";
 import millify from "millify";
 import DefaultImage from "../assets/images/house.jpg"
 
-const Property =({property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId}})=>(
-    <Link href={`/property/${externalId}`} passHref>
+const Property =({property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID}})=>(
+    <Link href={`/property/${externalID}`} passHref>
         <Flex flexWrap="wrap" w="420px" paddingTop="0" justifyContent="flex-start" cursor="pointer">
             <Box>
                 <Image src={coverPhoto? coverPhoto.url : DefaultImage} width={400} height={260} alt="house"/>
